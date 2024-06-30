bc815ae5b65fcd58697a17d4e66dc04deb4e7fa2
mrlthebest <115442027+mrlthebest@users.noreply.github.com>
2024-06-30 05:21:05 -0300

    community_scripts.lua
    Adicionado:
    
    Filtro de macros;
    Alterado a linguagem principal para inglês;
    Sistema de versão + aviso.

--- community_scripts.lua
+++ community_scripts.lua
@@ -1,39 +1,40 @@
+-- Initialize script_bot and script paths
 script_bot = {};
 script_path = '/scripts_storage/';
 script_path_json = script_path .. player:getName() .. '.json';
+
+-- Actual Version
+actualVersion = 0.2;
+
+-- Initialize script_manager with script cache
 script_manager = {
+    actualVersion = 0.2,
     _cache = {
-        Dbo = {
-
-        },
-        Nto = {
-
-        },
-        Tibia = {
-
-        },
+        Dbo = {},
+        Nto = {},
+        Tibia = {},
         PvP = {
             ['Attack Follow'] = {
                 url = 'https://raw.githubusercontent.com/mrlthebest/OTCV8/main/Follow_Attack.lua',
-                description = 'Script de follow attack.',
+                description = 'Follow attack script.',
                 author = 'VictorNeox',
                 enabled = false
             },
             ['Attack Target'] = {
                 url = 'https://raw.githubusercontent.com/ryanzin/OTCV8/main/ATTACK-TARGET.lua',
-                description = 'Script de hold target.',
+                description = 'Hold target script.',
                 author = 'Ryan',
                 enabled = false
             },
             ['Change Weapons'] = {
                 url = 'https://raw.githubusercontent.com/mrlthebest/OTCV8/main/change_weapon.lua',
-                description = 'Script de alterar arma por distancia do target.',
+                description = 'Script to change weapons based on target distance.',
                 author = 'mrlthebest',
                 enabled = false
             },
             ['Chase Icon'] = {
                 url = 'https://raw.githubusercontent.com/mrlthebest/OTCV8/main/chase_icon.lua',
-                description = 'Esse script ira criar um icone para desativar/ativar o chase.',
+                description = 'This script will create an icon to enable/disable chase.',
                 author = 'mrlthebest',
                 enabled = false
             },
@@ -41,7 +42,7 @@ script_manager = {
         Healing = {
             ['Heal Friend(Say)'] = {
                 url = 'https://raw.githubusercontent.com/mrlthebest/OTCV8/main/healFriend.lua',
-                description = 'Script de heal friend editavel por text edit.',
+                description = 'Script to heal a friend editable via text edit.',
                 author = 'mrlthebest',
                 enabled = false
             },
@@ -49,79 +50,79 @@ script_manager = {
         Utilities = {
             ['Buff'] = {
                 url = 'https://raw.githubusercontent.com/mrlthebest/OTCV8/main/buff.lua',
-                description = 'Script de buff por mensagem laranja.',
+                description = 'Script for buff via orange message.',
                 author = 'mrlthebest',
                 enabled = false
             },
             ['Bug Map'] = {
                 url = 'https://raw.githubusercontent.com/ryanzin/OTCV8/main/BUG-MAP.lua',
-                description = 'Script de bug map pc.',
+                description = 'PC map bug script.',
                 author = 'Ryan',
                 enabled = false
             },
             ['Sense'] = {
                 url = 'https://raw.githubusercontent.com/ryanzin/OTCV8/main/xNameSense.lua',
-                description = 'Script de sense, escreva "xNICK" que irá dar sense no nick, x0 para limpar o sense.',
+                description = 'Sense script, type "xNICK" to sense the nick, x0 to clear the sense.',
                 author = 'Ryan',
                 enabled = false
             },
             ['Bug Map Mobile'] = {
                 url = 'https://raw.githubusercontent.com/mrlthebest/OTCV8/main/Bug_Map_Mobile.lua',
-                description = 'Script de bug map mobile.',
+                description = 'Mobile map bug script.',
                 author = 'VictorNeox',
                 enabled = false
             },
             ['CaveBot/Targetbot Icon'] = {
                 url = 'https://raw.githubusercontent.com/mrlthebest/OTCV8/main/toggle.lua',
-                description = 'Esse script ira criar um icone para deixar on/off o cavebot.',
+                description = 'This script will create an icon to turn on/off the cavebot.',
                 author = 'F. Almeida',
                 enabled = false
             },
             ['Creature Health Percent'] = {
                 url = 'https://raw.githubusercontent.com/mrlthebest/OTCV8/main/creature_health.lua',
-                description = 'Script que ira mostrar a vida de qualquer creature na tela.',
+                description = 'Script that will show the health of any creature on the screen.',
                 author = 'mrlthebest',
                 enabled = false,
             },
             ['Loot Channel'] = {
                 url = 'https://raw.githubusercontent.com/mrlthebest/OTCV8/main/loot_channel.lua',
-                description = 'Cria um chat que ira mostrar os loots.',
+                description = 'Creates a chat to show loots.',
                 author = 'Dimitrys',
                 enabled = false
             },
             ['Open Main BP'] = {
                 url = 'https://raw.githubusercontent.com/mrlthebest/OTCV8/main/open%20main%20bp.lua',
-                description = 'Script de abrir as BPs principais.',
+                description = 'Script to open the main BPs.',
                 author = 'VivoDibra',
                 enabled = false
             },
             ['Follow Player'] = {
                 url = 'https://raw.githubusercontent.com/mrlthebest/OTCV8/main/follow%20player.lua',
-                description = 'Script de follow player.',
+                description = 'Script to follow player.',
                 author = 'VictorNeox',
                 enabled = false
             },
             ['PvP Mode Icon'] = {
                 url = 'https://raw.githubusercontent.com/mrlthebest/OTCV8/main/pvp_mode_icon.lua',
-                description = 'Esse script ira criar um icone para desativar/ativar a maozinha.',
+                description = 'This script will create an icon to enable/disable PvP mode.',
                 author = 'mrlthebest',
                 enabled = false
             },
             ['Spy Level'] = {
                 url = 'https://raw.githubusercontent.com/mrlthebest/OTCV8/main/storage_cavebot.lua',
-                description = 'Script de ver andar superior/inferior.',
+                description = 'Script to see upper/lower floors.',
                 author = 'vbot',
                 enabled = false
             },
             ['Storage CaveBot/TargetBot'] = {
                 url = 'https://raw.githubusercontent.com/mrlthebest/OTCV8/main/storage_cavebot.lua',
-                description = 'Script util para ter uma custom onde os cavebots nao se misturam pelos makers.',
+                description = 'Useful script to have custom cavebots that do not mix with makers.',
                 author = 'mrlthebest',
                 enabled = false
             },
             ['Widget Skills'] = {
                 url = 'https://raw.githubusercontent.com/mrlthebest/OTCV8/main/WidgetTrain.lua',
-                description = 'Script de widget onde mostra todas skills.',
+                description = 'Widget script that shows all skills.',
                 author = 'mrlthebest',
                 enabled = false
             },
@@ -129,6 +130,7 @@ script_manager = {
     },
 };
 
+-- Global functions and initializations
 _G = modules._G;
 context = _G.getfenv();
 g_resources = _G.g_resources;
@@ -136,24 +138,21 @@ listDirectoryFiles = g_resources.listDirectoryFiles;
 readFileContents = g_resources.readFileContents;
 fileExists = g_resources.fileExists;
 
-
----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+-- Create script directory if it doesn't exist
 if not fileExists(script_path) then
     g_resources.makeDir(script_path);
 end
 
----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+-- Function to read JSON file contents of scripts
 script_bot.readFileContents = function()
     local data = script_manager;
     if g_resources.fileExists(script_path_json) then
-        local content = g_resources.readFileContents(script_path_json)
-        local status, result = pcall(json.decode, content)
+        local content = g_resources.readFileContents(script_path_json);
+        local status, result = pcall(json.decode, content);
         if status then
             data = result;
         else
-            print("Erro ao decodificar o arquivo JSON:", result)
+            print("Error decoding JSON file:", result);
         end
     else
         script_bot.saveScripts();
@@ -161,8 +160,7 @@ script_bot.readFileContents = function()
     script_manager = data;
 end
 
----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+-- Function to save scripts to JSON file
 script_bot.saveScripts = function()
     local res = json.encode(script_manager, 4);
     local status, err = pcall(function() g_resources.writeFileContents(script_path_json, res) end);
@@ -171,14 +169,12 @@ script_bot.saveScripts = function()
     end
 end
 
----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+-- Define the UI for script list
 local script_add = [[
 UIWidget
   background-color: alpha
   focusable: true
   height: 30
-
 
   $focus:
     background-color: #00000055
@@ -189,8 +185,8 @@ UIWidget
     anchors.verticalCenter: parent.verticalCenter
     anchors.horizontalCenter: parent.horizontalCenter
 ]];
----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+-- Define the main script manager UI
 script_bot.widget = setupUI([[
 MainWindow
   !text: tr('Community Scripts')
@@ -204,7 +200,6 @@ MainWindow
     anchors.left: parent.left
     anchors.right: parent.right
     width: 180
-
 
   ScrollablePanel
     id: scriptList
@@ -234,29 +229,55 @@ MainWindow
     anchors.right: prev.right
     margin-left: 10
     margin-top: 6
-  
+
+  TextEdit
+    id: searchBar
+    anchors.left: parent.left
+    anchors.bottom: parent.bottom
+    margin-right: 5
+    width: 130
+
   Button
     id: closeButton
     !text: tr('Close')
     font: cipsoftFont
     anchors.right: parent.right
-    anchors.left: parent.left
+    anchors.left: searchBar.right
     anchors.bottom: parent.bottom
     size: 45 21
-    margin-top: 5
+    margin-bottom: 1
     margin-right: 5
-      
-]], g_ui.getRootWidget());
+    margin-left: 5
+]], g_ui.getRootWidget())
 script_bot.widget:hide();
----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-UI.Separator()
+
+UI.Separator();
+
+local updateLabel = UI.Label('Community Scripts. \n New version available, click "Update Files". \nVersion: ' .. actualVersion);
+updateLabel:setColor('yellow');
+updateLabel:hide();
+
+-- Define buttons and their functionalities
+--[[ Widget Button ]]--
 script_bot.buttonWidget = UI.Button('Script Manager');
 script_bot.buttonWidget:setColor('#d2cac5');
-
+--[[ Update Script Button ]]--
 script_bot.buttonRemoveJson = UI.Button('Update Files');
 script_bot.buttonRemoveJson:setColor('#d2cac5');
-script_bot.buttonRemoveJson:setTooltip('So clique aqui quando houver uma att.')
+script_bot.buttonRemoveJson:setTooltip('Click here only when there is an update.');
+script_bot.buttonRemoveJson.onClick = function(widget)
+    g_resources.deleteFile(script_path_json);
+    reload();
+end
 
+--[[ Close Widget Button ]] -- 
+script_bot.widget.closeButton:setTooltip('Close and add macros.');
+script_bot.widget.closeButton.onClick = function(widget)
+    reload();
+    script_bot.widget:hide();
+end
+
+--[[ Close Button On Widget ]] --
 script_bot.buttonWidget.onClick = function(widget)
     if script_bot.widget:isVisible() then
         reload();
@@ -265,32 +286,64 @@ script_bot.buttonWidget.onClick = function(widget)
     end
 end
 
-script_bot.widget.closeButton.onClick = function(widget)
-    reload();
-    script_bot.widget:hide();
+--[[ Search Bar Tooltip ]] --
+script_bot.widget.searchBar:setTooltip('Search macros.');
+
+UI.Separator();
+
+-- Function to filter scripts based on search text
+function script_bot.filterScripts(filterText)
+    for _, child in pairs(script_bot.widget.scriptList:getChildren()) do
+        local scriptName = child:getId();
+        if scriptName:lower():find(filterText:lower()) then
+            child:show();
+        else
+            child:hide();
+        end
+    end
 end
 
+-- Function to update script list based on the selected tab
+function script_bot.updateScriptList(tabName)
+    script_bot.widget.scriptList:destroyChildren();
+    local macrosCategory = script_manager._cache[tabName];
 
-script_bot.buttonRemoveJson.onClick = function(widget)
-    g_resources.deleteFile(script_path_json)
-    reload();
+    if macrosCategory then
+        for key, value in pairs(macrosCategory) do
+            local label = setupUI(script_add, script_bot.widget.scriptList);
+            label.textToSet:setText(key);
+            label.textToSet:setColor('#bdbdbd');
+            label:setTooltip('Description: ' .. value.description .. ' \nAuthor: ' .. value.author);
+
+            label.onClick = function(widget)
+                value.enabled = not value.enabled;
+                script_bot.saveScripts();
+                label.textToSet:setColor(value.enabled and 'green' or '#bdbdbd');
+                if value.enabled then
+                --loadRemoteScript(value.url);
+                end
+            end
+
+            if value.enabled then
+                label.textToSet:setColor('green');
+            end
+
+            label:setId(key);
+        end
+    end
 end
 
-UI.Separator()
-
----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+-- Function to load scripts
 script_bot.onLoading = function()
-    script_bot.widget.scriptList:destroyChildren()
+    script_bot.widget.scriptList:destroyChildren();
 
     local categories = {};
     for categoryName, categoryList in pairs(script_manager._cache) do
-        table.insert(categories, categoryName)
+        table.insert(categories, categoryName);
         for key, value in pairs(categoryList) do
             if value.enabled then
-                --loadRemoteScript(value.url)
-                modules.corelib.HTTP.get(value.url, function(script) 
-                    assert(loadstring(script))() 
+                modules.corelib.HTTP.get(value.url, function(script)
+                    assert(loadstring(script))();
                 end);
             end
         end
@@ -307,55 +360,39 @@ script_bot.onLoading = function()
                 local categoryName = categories[index];
                 local tab = script_bot.widget.macrosOptions:addTab(categoryName);
                 tab:setId(categoryName);
+                tab:setTooltip(categoryName .. ' Macros');
 
                 tab.onStyleApply = function(widget)
                     if script_bot.widget.macrosOptions:getCurrentTab() == widget then
                         widget:setColor('green');
                     else
-                        widget:setColor('white') ;
+                        widget:setColor('white');
                     end
-                end
-            end
-        end
-    end
-    local function updateScriptList(tabName)
-        script_bot.widget.scriptList:destroyChildren();
-        local macrosCategory = script_manager._cache[tabName];
-
-        if macrosCategory then
-            for key, value in pairs(macrosCategory) do
-                local label = setupUI(script_add, script_bot.widget.scriptList);
-                label.textToSet:setText(key);
-                label.textToSet:setColor('#bdbdbd');
-                label:setTooltip('Description: ' .. value.description .. ' \nAuthor: ' .. value.author);
-
-                label.onClick = function(widget)
-                    value.enabled = not value.enabled;
-                    script_bot.saveScripts();
-                    label.textToSet:setColor(value.enabled and 'green' or '#bdbdbd');
-                    if value.enabled then
-                    --loadRemoteScript(value.url)
-                    end
-                end
-
-                if value.enabled then
-                    label.textToSet:setColor('green');
                 end
             end
         end
     end
 
     local currentTab = script_bot.widget.macrosOptions:getCurrentTab().text;
-    updateScriptList(currentTab);
+    script_bot.updateScriptList(currentTab);
+
     script_bot.widget.macrosOptions.onTabChange = function(widget, tabName)
-        updateScriptList(tabName:getText());
+        script_bot.updateScriptList(tabName:getText());
+        script_bot.filterScripts(script_bot.widget.searchBar:getText());
+    end
+
+    script_bot.widget.searchBar.onTextChange = function(widget, text)
+        script_bot.filterScripts(text);
     end
 end
 
----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+-- Main looping
 do
     script_bot.readFileContents();
     script_bot.onLoading();
 end
 
+
+if script_manager.actualVersion ~= actualVersion then
+    updateLabel:show();
+end
