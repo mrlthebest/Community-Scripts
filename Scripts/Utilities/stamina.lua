691766edc2a78e4e1e04a991a306139eaa26d0da
mrlthebest <115442027+mrlthebest@users.noreply.github.com>
2024-07-06 03:18:12 -0300

    stamina.lua

---
+++ Scripts/Utilities/stamina.lua
@@ -0,0 +1,105 @@
+function staminaItems(parent)
+  if not parent then
+    parent = panel
+  end
+  local panelName = "staminaItemsUser"
+  local ui = setupUI([[
+Panel
+  height: 65
+  margin-top: 2
+
+  SmallBotSwitch
+    id: title
+    anchors.left: parent.left
+    anchors.right: parent.right
+    anchors.top: parent.top
+    text-align: center
+
+  HorizontalScrollBar
+    id: scroll1
+    anchors.left: parent.left
+    anchors.right: parent.horizontalCenter
+    anchors.top: title.bottom
+    margin-right: 2
+    margin-top: 2
+    minimum: 0
+    maximum: 42
+    step: 1
+    
+  HorizontalScrollBar
+    id: scroll2
+    anchors.left: parent.horizontalCenter
+    anchors.right: parent.right
+    anchors.top: prev.top
+    margin-left: 2
+    minimum: 0
+    maximum: 42
+    step: 1    
+
+  ItemsRow
+    id: items
+    anchors.left: parent.left
+    anchors.right: parent.right
+    anchors.top: prev.bottom
+  ]], parent)
+  ui:setId(panelName)
+
+  if not storage[panelName] then
+    storage[panelName] = {
+      min = 25,
+      max = 40,
+    }
+  end
+
+  local updateText = function()
+    ui.title:setText("" .. storage[panelName].min .. " <= stamina >= " .. storage[panelName].max .. "")  
+  end
+ 
+  ui.scroll1.onValueChange = function(scroll, value)
+    storage[panelName].min = value
+    updateText()
+  end
+  ui.scroll2.onValueChange = function(scroll, value)
+    storage[panelName].max = value
+    updateText()
+  end
+ 
+  ui.scroll1:setValue(storage[panelName].min)
+  ui.scroll2:setValue(storage[panelName].max)
+ 
+  ui.title:setOn(storage[panelName].enabled)
+  ui.title.onClick = function(widget)
+    storage[panelName].enabled = not storage[panelName].enabled
+    widget:setOn(storage[panelName].enabled)
+  end
+ 
+  if type(storage[panelName].items) ~= 'table' then
+    storage[panelName].items = { 11588 }
+  end
+ 
+  for i=1,5 do
+    ui.items:getChildByIndex(i).onItemChange = function(widget)
+      storage[panelName].items[i] = widget:getItemId()
+    end
+    ui.items:getChildByIndex(i):setItemId(storage[panelName].items[i])    
+  end
+ 
+  macro(1000, function()
+    if not storage[panelName].enabled or stamina() / 60 < storage[panelName].min or stamina() / 60 > storage[panelName].max then
+      return
+    end
+    local candidates = {}
+    for i, item in pairs(storage[panelName].items) do
+      if item >= 100 then
+        table.insert(candidates, item)
+      end
+    end
+    if #candidates == 0 then
+      return
+    end    
+    use(candidates[math.random(1, #candidates)])
+  end)
+end
+staminaItems(toolsTab)
+
+
