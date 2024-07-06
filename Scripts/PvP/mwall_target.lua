055f7de846c4b05f6a312f3f498009b7b102d025
mrlthebest <115442027+mrlthebest@users.noreply.github.com>
2024-07-06 03:26:43 -0300

    mwall_target.lua

---
+++ Scripts/PvP/mwall_target.lua
@@ -0,0 +1,27 @@
+local mwallId = 3180
+local offsetDirections = {
+    [North] = { 0, -2 },
+    [East] = { 2, 0 },
+    [South] = { 0, 2 },
+    [West] = { -2, 0 },
+    [NorthEast] = { 2, -2 },
+    [SouthEast] = { 2, 2 },
+    [SouthWest] = { -2, 2 },
+    [NorthWest] = { -2, -2 }
+}
+
+macro(1000, "Mwall Frente Target", function()
+    local target = g_game.getAttackingCreature()
+    if not target then
+        return
+    end
+
+    local targetPos = target:getPosition()
+    local targetDir = target:getDirection()
+
+    targetPos.x = targetPos.x + offsetDirections[targetDir][1]
+    targetPos.y = targetPos.y + offsetDirections[targetDir][2]
+    
+    local mwallTile = g_map.getTile(targetPos)
+    useWith(mwallId, mwallTile:getTopUseThing())
+end)
