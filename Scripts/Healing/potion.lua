fb651e6129ae7980f4bbb8c80a0a3814d394c8b6
mrlthebest. <115442027+mrlthebest@users.noreply.github.com>
2025-01-06 04:40:23 -0300

    Update potion.lua

--- Scripts/Healing/potion.lua
+++ Scripts/Healing/potion.lua
@@ -80,13 +80,16 @@ addScrollBar('potionHealth', 'Porcentagem Vida', 1, 100, 99, tabName, 'Porcentag
 addScrollBar('potionMana', 'Porcentagem Mana', 1, 100, 99, tabName, 'Porcentagem de mana para usar a potion.');
 addItem('potionLife', 'Potion Life', 11863, tabName, '');
 addItem('potionMana', 'Potion Mana', 11863, tabName, '');
+addScrollBar('potionDelay', 'Potion Delay', 0, 60, 1, tabName, 'Delay em segundos.');
 
 macro(100, "Potion", function()
     local selfHealth = hppercent();
     local selfMana = manapercent();
     if selfHealth < storage.scrollBarValues.potionHealth then
         useWith(storage.itemValues.potionLife, player)
+        delay(storage.scrollBarValues.potionDelay * 1000)
     elseif selfMana < storage.scrollBarValues.potionMana then
+        delay(storage.scrollBarValues.potionDelay * 1000)
         useWith(storage.itemValues.potionMana, player)
     end
 end, tabName);
