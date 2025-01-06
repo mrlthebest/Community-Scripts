990448f6d98fd4112679735c0eb59908f24c5f17
mrlthebest. <115442027+mrlthebest@users.noreply.github.com>
2025-01-06 07:24:57 -0300

    Update bug_map_mobile_kunai.lua

--- Scripts/Nto/bug_map_mobile_kunai.lua
+++ Scripts/Nto/bug_map_mobile_kunai.lua
@@ -1,37 +1,5 @@
 local macroName = 'Bug Map Kunai';
 local macroDelay = 100;
-local tabName = setDefaultTab('Main');
-
-itemWidget = [[
-Panel
-  height: 34
-  margin-top: 7
-  margin-left: 25
-  margin-right: 25
-
-  UIWidget
-    id: text
-    anchors.left: parent.left
-    anchors.verticalCenter: next.verticalCenter
-
-  BotItem
-    id: item
-    anchors.top: parent.top
-    anchors.right: parent.right
-]]
-
-storage.itemValues = storage.itemValues or {};
-addItem = function(id, title, defaultItem, dest, tooltip)
-    local widget = setupUI(itemWidget, dest)
-    widget.text:setText(title)
-    widget.text:setTooltip(tooltip)
-    widget.item:setTooltip(tooltip)
-    widget.item:setItemId(storage.itemValues[id] or defaultItem)
-    widget.item.onItemChange = function(widget)
-        storage.itemValues[id] = widget:getItemId()
-    end
-    storage.itemValues[id] = storage.itemValues[id] or defaultItem
-end
 
 addItem('kunaiId', 'ID Kunai', 11863, tabName, '')
 
