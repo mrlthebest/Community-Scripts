a1fb8b07c4fe9d9a2570d4293fcf797c25d1dc84
mrlthebest. <115442027+mrlthebest@users.noreply.github.com>
2025-01-06 06:53:02 -0300

    Update library.lua

--- library.lua
+++ library.lua
@@ -1,18 +1,5 @@
---[[
-
-  Scroll Bar Function
-
-  Id: Nome que irá ficar salvo no storage, usado para salvar os valrores.
-  Title: Titulo principal.
-  Min: Valor minimo.
-  Max: Valor máximo.
-  Default Value: Valor definido no começo.
-  Dest: Tab que irá ficar a scrollbar.
-  Tooltip: Tooltip que irá ficar no widget
-]]--
-
--- Scroll Bar Layout
-scrollBar = [[
+-- layout para scroll bar
+local scrollBarLayout = [[
 Panel
   height: 28
   margin-top: 3
@@ -23,7 +10,7 @@ Panel
     anchors.right: parent.right
     anchors.top: parent.top
     text-align: center
-    
+
   HorizontalScrollBar
     id: scroll
     anchors.left: parent.left
@@ -33,130 +20,61 @@ Panel
     minimum: 0
     maximum: 10
     step: 1
-]];
+]]
 
-storage.scrollBarValues = storage.scrollBarValues or {};
+-- inicializa a tabela de armazenamento
+storage.scrollBarValues = storage.scrollBarValues or {}
+
+-- função para adicionar uma scroll bar
 addScrollBar = function(id, title, min, max, defaultValue, dest, tooltip)
-    local value = storage.scrollBarValues[id] or defaultValue
-    local widget = setupUI(scrollBar, dest)
+    -- valida o valor inicial
+    local value = math.min(math.max(storage.scrollBarValues[id] or defaultValue, min), max)
+
+    -- cria o widget
+    local widget = setupUI(scrollBarLayout, dest)
+
+    -- valida se os widgets foram criados corretamente
+    if not widget.text or not widget.scroll then
+        error("Falha ao criar widgets para a scroll bar")
+        return
+    end
+
+    -- define propriedades do widget
     widget.text:setTooltip(tooltip)
     widget.scroll.onValueChange = function(scroll, value)
-        widget.text:setText(title)
-        widget.scroll:setText(value)
-        if value == 0 then
-            value = 1
-        end
+        widget.text:setText(title .. ": " .. value)
         storage.scrollBarValues[id] = value
     end
-    widget.scroll:setRange(min, max)
-    widget.scroll:setTooltip(tooltip)
+
+    widget.scroll:setMinimum(min)
+    widget.scroll:setMaximum(max)
     widget.scroll:setValue(value)
     widget.scroll.onValueChange(widget.scroll, value)
 end
 
-
---[[
-
-  Switch Bar Function
-
-  Id: Nome que irá ficar salvo no storage, usado para salvar os valrores.
-  Title: Titulo principal.
-  Default Value: Valor definido no começo(boolean).
-  Dest: Tab que irá ficar a scrollbar.
-  Tooltip: Tooltip que irá ficar no widget
-]]--
-
--- Switch Bar Layout
+-- layout para switch bar
 local switchBarLayout = [[
 BotSwitch
   height: 20
   margin-top: 7
-]];
+]]
 
+-- inicializa a tabela de armazenamento
 storage.switchStatus = storage.switchStatus or {}
 
+-- função para adicionar uma switch bar
 addSwitchBar = function(id, title, defaultValue, dest, tooltip)
+    -- cria o widget
     local widget = setupUI(switchBarLayout, dest)
+
+    -- define a lógica de clique no switch
     widget.onClick = function()
         widget:setOn(not widget:isOn())
         storage.switchStatus[id] = widget:isOn()
     end
+
+    -- define propriedades do widget
     widget:setText(title)
     widget:setTooltip(tooltip)
     widget:setOn(storage.switchStatus[id] or defaultValue)
 end
-
-
---[[
-
-  Item Widget Function
-
-  Id: Nome que irá ficar salvo no storage, usado para salvar os valrores.
-  Title: Titulo principal.
-  Default Item: Valor definido no começo(id).
-  Dest: Tab que irá ficar a scrollbar.
-  Tooltip: Tooltip que irá ficar no widget
-
-]]--
-
-itemWidget = [[
-Panel
-  height: 34
-  margin-top: 7
-  margin-left: 25
-  margin-right: 25
-
-  UIWidget
-    id: text
-    anchors.left: parent.left
-    anchors.verticalCenter: next.verticalCenter
-
-  BotItem
-    id: item
-    anchors.top: parent.top
-    anchors.right: parent.right
-]]
-
-storage.itemValues = storage.itemValues or {};
-addItem = function(id, title, defaultItem, dest, tooltip)
-    local widget = setupUI(itemWidget, dest)
-    widget.text:setText(title)
-    widget.text:setTooltip(tooltip)
-    widget.item:setTooltip(tooltip)
-    widget.item:setItemId(storage.itemValues[id] or defaultItem)
-    widget.item.onItemChange = function(widget)
-        storage.itemValues[id] = widget:getItemId()
-    end
-    storage.itemValues[id] = storage.itemValues[id] or defaultItem
-end
-
---[[
-
-  CheckBox Widget Function
-
-  Id: Nome que irá ficar salvo no storage, usado para salvar os valrores.
-  Title: Titulo principal.
-  Default Item: Valor definido no começo(boolean).
-  Dest: Tab que irá ficar a scrollbar.
-  Tooltip: Tooltip que irá ficar no widget
-
-]]--
-
-checkBoxWidget = [[
-CheckBox
-  width: 30
-]]
-
-storage.checkBoxStatus = storage.checkBoxStatus or {}
-addCheckBox = function(id, title, defaultBoolean, dest, tooltip)
-    local widget = setupUI(checkBoxWidget, dest)
-    widget:setText(title)
-    widget:setTooltip(tooltip)
-    widget.onCheckChange = function(widget, checked)
-        widget:setChecked(checked)
-        storage.checkBoxStatus[id] = checked;
-    end
-    widget:setChecked(storage.checkBoxStatus[id] or defaultBoolean)
-end
-
-
