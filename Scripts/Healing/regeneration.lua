292f7c73dbfb0bf21e692cc0ea9cdd3da10fe15a
mrlthebest. <115442027+mrlthebest@users.noreply.github.com>
2025-01-06 07:24:20 -0300

    Update regeneration.lua

--- Scripts/Healing/regeneration.lua
+++ Scripts/Healing/regeneration.lua
@@ -1,49 +1,5 @@
 local macroName = 'Regeneration';
 local macroDelay = 100;
-local tabName = setDefaultTab('Main');
-
-UI.Separator(tabName)
-scrollBar = [[
-Panel
-  height: 28
-  margin-top: 3
-
-  UIWidget
-    id: text
-    anchors.left: parent.left
-    anchors.right: parent.right
-    anchors.top: parent.top
-    text-align: center
-    
-  HorizontalScrollBar
-    id: scroll
-    anchors.left: parent.left
-    anchors.right: parent.right
-    anchors.top: prev.bottom
-    margin-top: 3
-    minimum: 0
-    maximum: 10
-    step: 1
-]];
-
-storage.scrollBarValues = storage.scrollBarValues or {};
-addScrollBar = function(id, title, min, max, defaultValue, dest, tooltip)
-    local value = storage.scrollBarValues[id] or defaultValue
-    local widget = setupUI(scrollBar, dest)
-    widget.text:setTooltip(tooltip)
-    widget.scroll.onValueChange = function(scroll, value)
-        widget.text:setText(title)
-        widget.scroll:setText(value)
-        if value == 0 then
-            value = 1
-        end
-        storage.scrollBarValues[id] = value
-    end
-    widget.scroll:setRange(min, max)
-    widget.scroll:setTooltip(tooltip)
-    widget.scroll:setValue(value)
-    widget.scroll.onValueChange(widget.scroll, value)
-end
 
 addTextEdit("Regeneration Spell", storage.regenerationSpell or "Regeneration Spell", function(widget, text)
     storage.regenerationSpell = text;
