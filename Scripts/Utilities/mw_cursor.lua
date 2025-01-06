e1ea5b6a7165c4172fe2c3894729f8962ebde405
mrlthebest. <115442027+mrlthebest@users.noreply.github.com>
2025-01-06 07:33:13 -0300

    Update mw_cursor.lua

--- Scripts/Utilities/mw_cursor.lua
+++ Scripts/Utilities/mw_cursor.lua
@@ -3,38 +3,6 @@ local macroDelay = 100;
 setDefaultTab(tabName);
 
 UI.Label('R MW Key', tabName)
-itemWidget = [[
-Panel
-  height: 34
-  margin-top: 7
-  margin-left: 25
-  margin-right: 25
-
-  UIWidget
-    id: text
-    anchors.left: parent.left
-    anchors.verticalCenter: next.verticalCenter
-
-  BotItem
-    id: item
-    anchors.top: parent.top
-    anchors.right: parent.right
-]]
-
-storage.itemValues = storage.itemValues or {};
-addItem = function(id, title, defaultItem, dest, tooltip)
-    local widget = setupUI(itemWidget, dest)
-    widget.text:setText(title)
-    widget.text:setTooltip(tooltip)
-    widget.item:setTooltip(tooltip)
-    widget.item:setItemId(storage.itemValues[id] or defaultItem)
-    widget.item.onItemChange = function(widget)
-        storage.itemValues[id] = widget:getItemId()
-    end
-    storage.itemValues[id] = storage.itemValues[id] or defaultItem
-end
-
-
 addItem('mwallId', 'MW ID', 110, tabName, '');
 
 hotkey('R', macroName, function()
