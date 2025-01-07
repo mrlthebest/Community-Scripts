92b43db6f4de8598301b148d6fc801e92c458664
mrlthebest. <115442027+mrlthebest@users.noreply.github.com>
2025-01-06 23:22:31 -0300

    Update community_scripts.lua

--- community_scripts.lua
+++ community_scripts.lua
@@ -1,22 +1,18 @@
 script_bot = {};
 
+-- Initialize script_bot and script paths
 tabName = nil;
 if ragnarokBot then
     script_path = ragnarokBot.path .. 'scripts_storage/';
     script_path_json = script_path .. player:getName() .. '.json';
     setDefaultTab('HP')
-    tabName = setDefaultTab('HP')
+    tabName = getTab('HP') or setDefaultTab('HP')
 else
     script_path = '/scripts_storage/';
     script_path_json = script_path .. player:getName() .. '.json';
     setDefaultTab('Main')
-    tabName = setDefaultTab('Tools')
+    tabName = getTab('Main') or setDefaultTab('Main')
 end
-
-
--- Initialize script_bot and script paths
-
-
 -- Actual Version
 actualVersion = 0.4;
 
